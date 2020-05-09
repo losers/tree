e5fc38e2833d770d9d86d622f054b1acafc811f6
varunkumarmedam <varunkumarmedam@gmail.com>
2020-05-10 01:09:58 +0530

    subtree created

--- src/views/Analytics/SubTree.vue
+++ src/views/Analytics/SubTree.vue
@@ -1,8 +1,52 @@
 <template>
   <div>
     <center>
-      <h3>We are working on it. We cant wait to see you</h3>
-      <img src="../../assets/working.jpg" width="200px">
+      <!-- <h3>We are working on it. We cant wait to see you</h3>
+      <img src="../../assets/working.jpg" width="200px">-->
+      <div class="row justify-content-center">
+        <vSelect :options="names" v-model="p1" class="col-3"></vSelect>
+        <button class="btn btn-success" @click="submit" :disabled="!p1">Search</button>
+      </div>
+
+      <TreeChart :json="tree" :images="images" style="padding-top:70px" />
     </center>
   </div>
 </template>
+
+<script>
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+import Store from "../../store/index";
+import Algos from "../../algos/analytics/relation-finder";
+import TreeChart from "@/components/TreeChart";
+
+export default {
+  data() {
+    return {
+      p1: null,
+      tree: {}
+    };
+  },
+  components: {
+    TreeChart,
+    vSelect
+  },
+  computed: {
+    names: {
+      get() {
+        return Store.getters.getAllMembers;
+      }
+    },
+    images: {
+      get() {
+        return Store.getters.images;
+      }
+    }
+  },
+  methods: {
+    submit() {
+      this.tree = Algos.getSubTree(Store.getters.getTreeData, this.p1.value);
+    }
+  }
+};
+</script>
