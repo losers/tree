ccf10b880fe0d1372145c8e1b402568249de2ef7
M GURU SAI JAYA PRAKASH <gurusai.m@samsung.com>
2020-09-23 03:35:34 +0530

    Timeline Updates

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,25 @@
 module.exports = {
-    pages: {
-        index: {
-            entry: 'src/pages/index/main.js',
-            template: 'public/index.html',
-            filename: 'index.html',
+    devServer: {
+        proxy: {
+            "/api": {
+                target: 'http://localhost:5000',
+                secure : false,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
         },
-        app: {
-            entry: 'src/pages/list/list.js',
-            template: 'public/app.html',
-            filename: 'app.html',
+    },
+        pages: {
+            index: {
+                entry: 'src/pages/index/main.js',
+                template: 'public/index.html',
+                filename: 'index.html',
+            },
+            app: {
+                entry: 'src/pages/list/list.js',
+                template: 'public/app.html',
+                filename: 'app.html',
+            }
         }
-      }
-};
+}
