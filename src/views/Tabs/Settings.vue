cbb75705c646eedbd2a1beb46ecbcd703f16e5f2
M GURU SAI JAYA PRAKASH <gurusai.m@samsung.com>
2021-02-03 02:55:31 +0530

    Paypal Integ

--- src/views/Tabs/Settings.vue
+++ src/views/Tabs/Settings.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
+    <button @click="initiatePaymentGateway">Paypal</button>
     <section v-if="form.error">
       <center>
-        <img
-          src="@/assets/no-data.png"
-          style="height: 140px; margin-top: 70px"
-        />
+        <img src="@/assets/no-data.png" style="height: 140px; margin-top: 70px" />
         <h4 style="color: black; margin-top: 30px">You dont have access buddy</h4>
       </center>
     </section>
@@ -21,18 +19,12 @@
         "
       >
         <center>
-          <div
-            class="spinner-border spinner-border-sm"
-            style="width: 5rem; height: 5rem"
-          ></div>
+          <div class="spinner-border spinner-border-sm" style="width: 5rem; height: 5rem"></div>
         </center>
       </div>
 
       <!-- Settings Body -->
-      <div
-        v-else
-        :style="{ padding: $device.mobile ? '0 50px 50px 50px' : '0 200px' }"
-      >
+      <div v-else :style="{ padding: $device.mobile ? '0 50px 50px 50px' : '0 200px' }">
         <p style="float: right">
           <strong>Created On :</strong>
           {{ new Date(data.created_at).toDateString() }}
@@ -40,12 +32,7 @@
         <h4>General Settings</h4>
         <form v-on:submit.prevent="updateSettings">
           <div class="form-inline row">
-            <label
-              class="col"
-              style="justify-content: left"
-              v-if="!$device.mobile"
-              >Display Title :</label
-            >
+            <label class="col" style="justify-content: left" v-if="!$device.mobile">Display Title :</label>
             <input
               type="text"
               class="form-control col-sm-9"
@@ -61,8 +48,7 @@
               class="col"
               style="justify-content: left"
               v-if="!$device.mobile"
-              >Surname :</label
-            >
+            >Surname :</label>
             <input
               v-model="data.surname"
               type="text"
@@ -77,8 +63,7 @@
               class="col"
               style="justify-content: left"
               v-if="!$device.mobile"
-              >Admin PIN :</label
-            >
+            >Admin PIN :</label>
             <input
               v-model="data.pin"
               type="number"
@@ -89,10 +74,7 @@
               required
             />
           </div>
-          <div
-            v-if="data.pin == '1234' || data.pin == '0000'"
-            class="mt-3 text-warning"
-          >
+          <div v-if="data.pin == '1234' || data.pin == '0000'" class="mt-3 text-warning">
             <div class="mb-2">Admin pin can be easily guessed.</div>
           </div>
           <div class="form-inline row">
@@ -101,8 +83,7 @@
               class="col"
               style="justify-content: left"
               v-if="!$device.mobile"
-              >View-Only PIN :</label
-            >
+            >View-Only PIN :</label>
             <input
               v-model="data.view_pin"
               type="number"
@@ -112,14 +93,12 @@
               onkeypress="if(this.value.length==4) return false;"
             />
           </div>
-          <div
-            v-if="data.view_pin == data.pin && data.pin"
-            class="mt-3 text-danger"
-          >
+          <div v-if="data.view_pin == data.pin && data.pin" class="mt-3 text-danger">
             <div class="mb-2">Admin PIN and View-Only PIN cannot be same</div>
           </div>
           <h4 class="contact-details">
-            Contact Details <span>(optional)</span>
+            Contact Details
+            <span>(optional)</span>
           </h4>
           <div class="form-inline row">
             <label
@@ -127,8 +106,7 @@
               class="col"
               style="justify-content: left"
               v-if="!$device.mobile"
-              >Your Name :</label
-            >
+            >Your Name :</label>
             <input
               type="text"
               class="form-control col-sm-9"
@@ -144,8 +122,7 @@
               class="col"
               style="justify-content: left"
               v-if="!$device.mobile"
-              >Your Email :</label
-            >
+            >Your Email :</label>
             <input
               type="email"
               class="form-control col-sm-9"
@@ -189,11 +166,7 @@
             <code>{{ this.$route.params.id }}</code> in the input box to delete
             this family tree permanently.
           </h6>
-          <input
-            class="form-control input-lg"
-            type="text"
-            v-model="deleteSurname"
-          />
+          <input class="form-control input-lg" type="text" v-model="deleteSurname" />
           <button
             :disabled="deleteSurname != this.$route.params.id || deletingFamily"
             class="btn btn-danger mt-3"
@@ -203,16 +176,13 @@
               class="spinner-border spinner-border-sm"
               v-show="deletingFamily"
               style="margin-right: 8px"
-            ></span
-            >Delete Permanently
+            ></span>Delete Permanently
           </button>
         </div>
 
         <div class="mt-5">
           Any Trouble ?
-          <a href="mailto:bloodline.helpline@gmail.com"
-            >bloodline.helpline@gmail.com</a
-          >
+          <a href="mailto:bloodline.helpline@gmail.com">bloodline.helpline@gmail.com</a>
         </div>
       </div>
     </section>
@@ -230,28 +200,57 @@ export default {
       form: {
         error: false,
         loading: true,
-        isUpdating: false,
+        isUpdating: false
       },
       data: {
         contact: {},
-        pin: "",
+        pin: ""
       },
       deletingFamily: false,
-      deleteSurname: "",
+      deleteSurname: ""
     };
   },
   mounted() {
+    // let url1 = `${ProdData.getHostURL()}/pay/${this.$route.params.id}/profile`;
+    // Axios.post(url1, {})
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(err => console.log(err))
+    //   .finally(() => console.log("daskna"));
+    if (this.$route.query.paymentId && this.$route.query.PayerID) {
+      let url = `${ProdData.getHostURL()}/pay/${this.$route.params.id}`;
+      let params = {
+        payment_id: this.$route.query.paymentId,
+        payer_id: this.$route.query.PayerID
+      };
+      Axios.put(url, params)
+        .then(data => {
+          console.log(data.data);
+        })
+        .catch(err => console.log(err))
+        .finally(() => console.log("Placeholder"));
+    }
     Axios.get(ProdData.getHostURL() + "/meta/get/" + this.$route.params.id)
-      .then((data) => {
+      .then(data => {
         this.data = data.data;
-        if(!this.data.contact){
+        if (!this.data.contact) {
           this.data.contact = {};
         }
       })
-      .catch((err) => (this.form.error = err))
+      .catch(err => (this.form.error = err))
       .finally(() => (this.form.loading = false));
   },
   methods: {
+    initiatePaymentGateway() {
+      let url = `${ProdData.getHostURL()}/pay/${this.$route.params.id}`;
+      Axios.post(url, {})
+        .then(data => {
+          window.open(data.data.url, "_self");
+        })
+        .catch(err => console.log(err))
+        .finally(() => console.log("daskna"));
+    },
     updateSettings() {
       this.form.isUpdating = true;
       Axios.put(ProdData.getHostURL() + "/meta/update", {
@@ -260,28 +259,28 @@ export default {
         created_at: this.data.created_at,
         pin: this.data.pin,
         view_pin: this.data.view_pin,
-        contact: this.data.contact,
+        contact: this.data.contact
       })
         .then(() => {
           if (Store.state.title != this.data.title) {
             Store.dispatch("setTitle", this.data.title);
           }
         })
-        .catch((err) => (this.form.error = err))
+        .catch(err => (this.form.error = err))
         .finally(() => (this.form.isUpdating = false));
     },
     deleteFamily() {
       this.deletingFamily = true;
       Axios.delete(ProdData.getHostURL() + "/meta", {
-        data: { surname: this.$route.params.id },
+        data: { surname: this.$route.params.id }
       })
-        .then((data) => console.log(data))
-        .catch((err) => console.log(err))
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
         .finally(() => {
           this.$router.push({ name: "Home" });
         });
-    },
-  },
+    }
+  }
 };
 </script>
 
